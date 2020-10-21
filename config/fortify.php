151946e9158e1e9a27714e706ae3afc7cb601248
Muhamad Iqbal Tawakal <tawakalisme@gmail.com>
2020-10-21 08:33:34 +0700

    Edit ENV

--- config/fortify.php
+++ config/fortify.php
@@ -84,7 +84,7 @@ return [
     */
 
     'features' => [
-        // Features::registration(),
+        Features::registration(),
         Features::resetPasswords(),
         // Features::emailVerification(),
         Features::updateProfileInformation(),
